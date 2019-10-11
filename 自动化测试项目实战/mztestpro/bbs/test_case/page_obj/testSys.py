b0021fe7746eb181c93f6721ec47042949126341
test02 <test02@126.com>
2019-10-11 10:36:33 +0800

    12
    12

---
+++ 自动化测试项目实战/mztestpro/bbs/test_case/page_obj/testSys.py
@@ -0,0 +1,9 @@
+import sys
+
+sys.path.append("./models")
+sys.path.append("./page_obj")
+from models import myunit, function
+from page_obj.loginPage import login
+
+if __name__ == "__main__":
+    print("ok")
