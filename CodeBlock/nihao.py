d5cfb32b24d310ac495a149f89ca83db1b626ac3
test02 <test02@126.com>
2019-11-13 17:12:56 +0800

    1

--- CodeBlock/nihao.py
+++ CodeBlock/nihao.py
@@ -1,2 +1,23 @@
-print('你好')
-print("你好")
+# i = 0
+
+# class ClaA():
+
+#     def printA(self):
+#         global i
+#         print(i)
+#         i+=1
+
+# if __name__ == "__main__":
+#     ClaA().printA()
+
+#     ClaA().printA()
+#     ClaA().printA()
+# i = 1
+# while i < 101:
+#     print("--%03d--" %i)
+#     i+=1
+
+i = 1
+while i <= 16:  # 跟最后一个测试案例的数字一致，增加案例时，这里要改
+    print("test_%d_login" % i)
+    i += 1
